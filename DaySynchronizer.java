beba2f5583a26a8e319d961684c95ef7cadedeb0
Wenshu_PK <puvit.kit@student.mahidol.edu>
2025-10-22 12:32:03 +0700

    Update DaySynchronizer.java

--- DaySynchronizer.java
+++ DaySynchronizer.java
@@ -4,11 +4,32 @@
  */
 package Project2;
 
+import java.util.concurrent.*;
+
 /**
  *
  * @author puvit
  */
 public class DaySynchronizer {
-    
-}
+    private final CyclicBarrier barrierStart;
+    private final CyclicBarrier barrierAfterSuppliers;
+    private final CyclicBarrier barrierAfterShipping;
 
+    public DaySynchronizer(int totalThreadsPlusMain) {
+        barrierStart = new CyclicBarrier(totalThreadsPlusMain);
+        barrierAfterSuppliers = new CyclicBarrier(totalThreadsPlusMain);
+        barrierAfterShipping = new CyclicBarrier(totalThreadsPlusMain);
+    }
+
+    public void awaitStart() throws InterruptedException, BrokenBarrierException {
+        barrierStart.await();
+    }
+
+    public void awaitAfterSuppliers() throws InterruptedException, BrokenBarrierException {
+        barrierAfterSuppliers.await();
+    }
+
+    public void awaitAfterShipping() throws InterruptedException, BrokenBarrierException {
+        barrierAfterShipping.await();
+    }
+}
