f2fa3e1ab9328ec7fae227baae2e6b6f18a92cf8
Wenshu_PK <puvit.kit@student.mahidol.edu>
2025-10-22 12:32:56 +0700

    Update SupplierThread.java

--- SupplierThread.java
+++ SupplierThread.java
@@ -4,11 +4,50 @@
  */
 package Project2;
 
+import java.util.*;
+
+
 /**
  *
  * @author puvit
  */
-public class SupplierThread {
-    
+public class SupplierThread extends Thread {
+    private final List<Warehouse> warehouses;
+    private final int minSupply;
+    private final int maxSupply;
+    private final int days;
+    private final DaySynchronizer sync;
+    private final Random rng = new Random();
+
+    public SupplierThread(String name, List<Warehouse> warehouses,
+                          int minSupply, int maxSupply, int days, DaySynchronizer sync) {
+        super(name);
+        this.warehouses = warehouses;
+        this.minSupply = minSupply;
+        this.maxSupply = maxSupply;
+        this.days = days;
+        this.sync = sync;
+    }
+
+    @Override
+    public void run() {
+        for (int day = 1; day <= days; day++) {
+            try {
+                sync.awaitStart();
+
+                int amount = minSupply + rng.nextInt(maxSupply - minSupply + 1);
+                Warehouse w = warehouses.get(rng.nextInt(warehouses.size()));
+                w.put(amount);
+                System.out.printf("%s >> put %d materials\t %s balance = %d%n",
+                        getName(), amount, w.getName(), w.getBalance());
+
+                sync.awaitAfterSuppliers();
+                sync.awaitAfterShipping();
+            } catch (Exception e) {
+                System.out.printf("%s >> interrupted/broken at day %d%n", getName(), day);
+                return;
+            }
+        }
+    }
 }
 
