d647975007c7190b2976330e0ddea7e2fb3cab10
Anunstu <anun.lue@student.mahidol.edu>
2025-10-22 22:20:07 +0700

    Add files via upload

--- FactoryThread.java
+++ FactoryThread.java
@@ -14,23 +14,49 @@ public class FactoryThread extends Thread{
     private int maxProduction;
     private int products;
     private CyclicBarrier barrier;
-    private MyMonitor monitor;
-    private int totalProduction;
+    private MyMonitor monitorS, monitorM;
+    private int totalProducts;
     private int unshipped;
     private ArrayList<WareHouse> warehouses;
     private ArrayList<Freight> freights;
-    public FactoryThread(int max, ArrayList<WareHouse> w, ArrayList<Freight> f)   {maxProduction = max; warehouses = w; freights = f;}
+    private int days;
+    public FactoryThread(String n, int d, int max, ArrayList<WareHouse> w, ArrayList<Freight> f)   {super(n); days = d; maxProduction = max; warehouses = w; freights = f;}
     public void setMonitor(MyMonitor m)   {monitor = m;}
     public void setBarrier(CyclicBarrier ba)    {barrier = ba;}
     @Override
     public void run()
     {
-        Random rand = new Random();
-        int selectw = rand.nextInt(0, warehouses.size()-1);
-        int selectf = rand.nextInt(0, freights.size()-1);
-        products = rand.nextInt(1, maxProduction);
-        products = warehouses.get(selectw).get(products);
-        
+        while(true){
+            monitorS.waitForInput();
+            Random rand = new Random();
+            int selectw = rand.nextInt(0, warehouses.size()-1);
+            int selectf = rand.nextInt(0, freights.size()-1);
+            products = rand.nextInt(1, maxProduction);
+            products = warehouses.get(selectw).get(products);
+            getMats(selectw);
+            int c =-1;
+            try{c = barrier.await();}catch(Exception e){}
+            if(c==0)
+            {
+                System.out.println(Thread.currentThread().getName() + ">>");
+            }
+            totalProducts = unshipped + products;
+            totalToShip();
+            try{barrier.await();}catch(Exception e){}
+            shippingProduct(selectf);
+            try{barrier.await();}catch(Exception e){}
+            unshippedProducts();
+            days--;
+            c = -1;
+            try{c = barrier.await();}catch(Exception e){}
+            if(c==0)
+            {
+                monitorM.wakeUpThreads();
+            }
+            if(days == 1){
+                break;
+            }
+        }
             
         
         
@@ -38,5 +64,23 @@ public class FactoryThread extends Thread{
             
         
     }
-    
+    public void getMats(int s)
+    {
+        System.out.printf("%s  >>  get %3d materials      %s balance = %5d\n",Thread.currentThread().getName(), products, warehouses.get(s).getName(), warehouses.get(s).get());
+    }
+    public void totalToShip()
+    {
+        System.out.printf("%s  >>  total product to ship = %5d\n", Thread.currentThread().getName(), totalProducts);
+    }
+    public void shippingProduct(int s)
+    {
+        int remain = freights.get(s).put(totalProducts);
+        int shipped = totalProducts - remain;
+        System.out.printf("%s  >>  ship %4d products      %s remaining capacity = %5d\n", Thread.currentThread().getName(), shipped, freights.get(s).getName(), freights.get(s).get());
+        unshipped = remain;
+    }
+    public void unshippedProducts()
+    {
+        System.out.printf("%s  >>  unshipped products = %5d\n", Thread.currentThread().getName(), unshipped);
+    }
 }
