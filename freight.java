aeac88b99c7792507a8de2fdacb1c2c14f91f7d8
piya-co <piyawat.jar@student.mahidol.edu>
2025-11-01 14:06:21 +0700

    Update freight.java
    package name

--- freight.java
+++ freight.java
@@ -1,6 +1,11 @@
-package GPRO2JAVA;
+package Project2_6713249
 
-
+//Anun Luechaphongthip         6713253
+//Puvit Kitiwongpaisan         6713246
+//Kanapod Lamthong             6713220
+//Piyawat Jaroonchaikhanakit   6713240
+//Sawana Thiputhai             6713249
+    
 public class freight {
     private final int freight_num;
     private final int freight_cap;
