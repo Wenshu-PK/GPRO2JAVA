46aa43731115f5adb0b7f1278014c59f2bc2fc8b
piya-co <piyawat.jar@student.mahidol.edu>
2025-11-01 14:07:54 +0700

    Update SupplierThread.java

--- SupplierThread.java
+++ SupplierThread.java
@@ -1,4 +1,4 @@
-package Project2_6713249
+package Project2_6713249;
 
 //Anun Luechaphongthip         6713253
 //Puvit Kitiwongpaisan         6713246
@@ -74,5 +74,6 @@ public void run() {
 }
 
 }
+
 
 
